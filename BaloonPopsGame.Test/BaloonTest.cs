32676e74409c06da396c441e3afd10dde694ae61
Petar Petkov <petar.b.petkov@gmail.com>
2015-05-06 00:58:59 +0300

    Small changes

---
+++ BaloonPopsGame.Test/BaloonTest.cs
@@ -0,0 +1,35 @@
+﻿namespace BaloonsPopGame.Tests
+{
+    using System;
+    using BalloonsPops.Data;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    [TestClass]
+    public class BaloonTest
+    {
+       [TestMethod]
+        public void Test_ComapreTwoSameBaloons_ShouldReturnTrue()
+        {
+            var color1 = new Color(ConsoleColor.Black, 2);
+            var baloon1 = new Baloon("x", color1);
+            var baloon2 = new Baloon("x", color1);
+            Assert.AreEqual(baloon1, baloon2);
+        }
+
+        [TestMethod]
+        public void Test_CompareTwoDifferentBaloons_ShouldReturnFalse()
+        {
+            var color1 = new Color(ConsoleColor.Black, 2);
+            var baloon1 = new Baloon("x", color1);
+            var baloon2 = new Baloon("y", color1);
+            Assert.AreNotEqual(baloon1, baloon2);
+        }
+
+        [TestMethod]
+        public void Test_CompareWithNonBaloonObjet_ShouldReturnFalse()
+        {
+            var color1 = new Color(ConsoleColor.Black, 2);
+            var baloon1 = new Baloon("y", color1);
+            Assert.AreNotEqual(baloon1, color1);
+        }
+    }
+}
---
+++ BaloonPopsGame.Test/PlayerTest.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace BaloonsPopGame.Tests
+{
+    using BalloonsPops.Data;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    [TestClass]
+    public class PlayerTest
+    {
+        [TestMethod]
+        public void Test_CreateValidPlayer_ShouldHaveCorrectValues()
+        {
+            var player = new Player("pesho");
+            Assert.AreEqual("pesho", player.Name);
+            Assert.AreEqual(0, player.CurrentMoves);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "The name must be at least 2 characters")]
+        public void Test_SetInvalidUsername_ShouldThrowExeption()
+        {
+            var player = new Player("g");
+        }
+    }
+}
