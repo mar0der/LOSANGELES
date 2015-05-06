1a2c85e0fc29b8028dad25508d05519dbd0fbfa4
valkone <valkb7b@gmail.com>
2015-05-06 15:05:11 +0300

    Game board unit tests added

--- BaloonPopsGame.Test/GameBoardTest.cs
+++ BaloonPopsGame.Test/GameBoardTest.cs
@@ -1,14 +1,34 @@
-﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BalloonsPops.Data;
+using BalloonsPops.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
-//namespace BaloonsPopGame.Tests
-//{
-//    [TestClass]
-//    public class GameBoardTest
-//    {
-//        [TestMethod]
-//        public void Test_MoveElementDown_SholdBeMoved()
-//        {
+namespace BaloonsPopGame.Tests
+{
+    [TestClass]
+    public class GameBoardTest
+    {
 
-//        }
-//    }
-//}
+        [TestMethod]
+        public void Test_MoveElementDown_SholdReturnTrue()
+        {
+            Color color = new Color(ConsoleColor.Black, 1);
+            IEntity fullEntity = new Baloon("1", color);
+            IEntity emptyEntity = new Baloon(".", color);
+            IEntity[,] entities = new Baloon[2, 2];
+
+            entities[0, 0] = fullEntity;
+            entities[0, 1] = fullEntity;
+            entities[1, 0] = emptyEntity;
+            entities[1, 1] = fullEntity;
+
+            GameBoard gameBoard = new GameBoard(entities);
+            gameBoard.Drop();
+
+            Assert.AreEqual(".", gameBoard.Entities[0, 0].Symbol);
+            Assert.AreEqual("1", gameBoard.Entities[0, 1].Symbol);
+            Assert.AreEqual("1", gameBoard.Entities[1, 0].Symbol);
+            Assert.AreEqual("1", gameBoard.Entities[1, 1].Symbol);
+        }
+    }
+}
