88c4ee7ca2887049135ed2437989450d91a9bb20
Petar Petkov <petar.b.petkov@gmail.com>
2015-05-06 19:43:46 +0300

    GameBoardGenerator unit tests added

---
+++ BaloonPopsGame.Test/GameBoardGeneratorTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.CodeDom;
+using BalloonsPops.Core;
+using BalloonsPops.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaloonPopsGame.Test
+{
+    [TestClass]
+    public class GameBoardGeneratorTest
+    {
+        [TestMethod]
+        public void Test_GameBoardGenerator_ShouldFillAllFieldsWithBaloons()
+        {
+            var gameBoard = GameBoardGenerator.GenerateGameBoard(20, 20, 4);
+            for (int row = 0; row < gameBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < gameBoard.GetLength(1); col++)
+                {
+                    Assert.IsInstanceOfType(gameBoard[row, col], typeof(Baloon));
+                }
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void Test_GameBoardGeneratorWithIvalidParameters_ShouldThrowAnExeption()
+        {
+            GameBoardGenerator.GenerateGameBoard(-20, 20, 4);
+
+        }
+    }
+}
--- BaloonPopsGame/Core/GameBoardGenerator.cs
+++ BaloonPopsGame/Core/GameBoardGenerator.cs
@@ -1,4 +1,6 @@
-﻿namespace BalloonsPops.Core
+﻿using System;
+
+namespace BalloonsPops.Core
 {
     using BalloonsPops.Data;
 
@@ -7,9 +9,16 @@
 
         public static Baloon[,] GenerateGameBoard(int boardHeight, int boardWidth, int maxCoulorCount)
         {
+
+            if (boardHeight <= 0 || boardWidth <= 0 || maxCoulorCount <= 0)
+            {
+                throw new ApplicationException("The board size and color count must be positive number");
+            }
+
             var gameBoard = new Baloon[boardHeight, boardWidth];
             Baloon newBaloon;
             Color color;
+
             for (int row = 0; row < boardHeight; row++)
             {
                 for (int col = 0; col < boardWidth; col++)
